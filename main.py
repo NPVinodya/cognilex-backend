10ec7d05d43cab6f456e5995d6c71b2bb0096839
Prabani Vinodya <prabanivinodya@gmail.com>
2026-04-28 17:33:20 +0530

    feat: update CORS middleware to include additional production domain

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app = FastAPI(title="CogniLex Backend",lifespan=lifespan)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://www.cognilex.systems"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://www.cognilex.systems","https://cognilex.systems"],
     allow_credentials=True,
     allow_methods=["*"],
 )
